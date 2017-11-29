7766e05933c1cead46c63baa07507b7ace81b4e3
Kevin Cai <kevincai1979@gmail.com>
2017-11-29 12:08:17 -0800

    add code for reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  var reverseStr = '';
+  for(var i = str.length -1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+}
 
 module.exports = reverse;
