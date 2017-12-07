48a1ece600f988500df1b5f884d4f61caf21dfbd
Kevin Cai <kevincai1979@gmail.com>
2017-12-06 17:47:46 -0800

    add solutiuon for capitalize challenge

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,26 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let newStr = '';
+
+  if (str[0].match(/[a-zA-Z]/)) {
+    newStr = str[0].toUpperCase();
+  } else {
+    newStr = str[0];
+  }
+
+  console.log(newStr);
+
+  for (let i = 1; i < str.length; i ++ ) {
+    if (str[i -1].match(/[^a-zA-Z]/) && str[i].match(/[a-zA-Z]/)) {
+      newStr = newStr + str[i].toUpperCase();
+    } else {
+      newStr = newStr + str[i];
+    }
+  }
+
+  return newStr;
+}
 
 module.exports = capitalize;
