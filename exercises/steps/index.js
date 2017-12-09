19705fffb95bd80dcd4767b6030b8dd99083aaed
Kevin Cai <kevincai1979@gmail.com>
2017-12-08 17:34:15 -0800

    add solution for steps challenge

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,11 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+
+  for (let i = 1; i <= n; i ++) {
+    console.log('#'.repeat(i) + ' '.repeat(n - i));
+  }
+}
 
 module.exports = steps;
