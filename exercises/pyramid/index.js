02f91d5bfcd5818994dde848b8fd72d463cddd26
Kevin Cai <kevincai1979@gmail.com>
2017-12-12 17:02:41 -0800

    add recusion solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -40,4 +40,26 @@ function pyramid(n) {
   }
 }
 
+// Below is the solution with recusion
+
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n -1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
+}
+
 module.exports = pyramid;
