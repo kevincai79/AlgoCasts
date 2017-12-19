e316bb1a9b25b16518655bf6d0d58839abd22cca
Kevin Cai <kevincai1979@gmail.com>
2017-12-19 10:21:25 -0800

    add solution for fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,19 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  let fib = [];
+
+  for (let i = 0; i <= n; i++) {
+    if (i === 0) {
+      fib.push(0);
+    } else if (i === 1) {
+      fib.push(1);
+    } else {
+      fib.push(fib[i-1] + fib[i-2]);
+    }
+  }
+  return fib[n];
+}
 
 module.exports = fib;
