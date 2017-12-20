cdc67eedb68ea3f894d74bd4bafcde2103201701
Kevin Cai <kevincai1979@gmail.com>
2017-12-20 11:23:27 -0800

    add memoize solution to make the recusion solution works with better run time

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -26,15 +26,45 @@ function fib(n) {
 // Below is the solution 2 with recusion
 
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  }
-
-  if (n === 1) {
-    return 1;
+  // if (n === 0) {
+  //   return 0;
+  // }
+  //
+  // if (n === 1) {
+  //   return 1;
+  // }
+  if (n < 2) {
+    return n;
   }
 
   return fib(n -1) + fib(n -2)
 }
+
+// Below is the memoize solution to make the recusion solution runtime better
+
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    };
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+// Below line 60 and line 68 can change the slowFib to fib
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n -1) + fib(n -2)
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
